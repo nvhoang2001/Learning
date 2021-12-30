ec51734f7aa1042eaa1870ed892c328c78ce52e6
nvhoang2001 <ngoviethoang2424@gmail.com>
2021-12-30 21:36:19 +0700

    Vue basic, learn how to render List and element conditionally

---
+++ Vue - section-03-starting-code/app.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+	data() {
+		return { goals: [], enteredGoal: "" };
+	},
+	methods: {
+		addGoal() {
+			this.goals.push(this.enteredGoal);
+			this.enteredGoal = "";
+		},
+		removeGoal(idx) {
+			this.goals.splice(idx, 1);
+		},
+	},
+});
+
+app.mount("#user-goals");
