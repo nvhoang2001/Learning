d9bde7caa4c78b3fa2b57d818190ae87786466f0
nvhoang2001 <ngoviethoang2424@gmail.com>
2021-12-30 16:19:44 +0700

    VueJs basic, more about interpolation, first touch on watchers, computed properties, event in Vue

---
+++ Vue - section-02-starting-code-02/app.js
@@ -0,0 +1,60 @@
+const app = Vue.createApp({
+	data() {
+		return {
+			counter: 0,
+			name: "",
+			confirmName: "",
+			// fullname: "",
+			lastName: "",
+		};
+	},
+	watch: {
+		counter(value) {
+			if (value > 50) {
+				this.counter = 0;
+			}
+		},
+		// name(value, oldValue) {
+		// 	console.log(oldValue);
+		// 	console.log(value);
+		// 	if (value === "") {
+		// 		this.fullname = "";
+		// 	} else {
+		// 		this.fullname = "123" + " " + value;
+		// 	}
+		// },
+	},
+	computed: {
+		fullname() {
+			console.log("Name is leveraging");
+			return this.name === "" || this.lastName === "" ? "" : `${this.lastName} ${this.name}`;
+		},
+	},
+	methods: {
+		increase(amount) {
+			this.counter += amount;
+		},
+		decrease(amount) {
+			this.counter -= amount;
+		},
+		setName(e) {
+			this.name = e.target.value;
+		},
+		submitForm(e) {
+			// e.preventDefault();
+			console.log(e);
+		},
+		updateName() {
+			this.confirmName = this.name;
+		},
+		resetInput() {
+			this.name = "";
+		},
+		outputFullname() {
+			console.log("Name is leveraging");
+			return this.name === "" ? "" : `Ngo Viet ${this.name}`;
+		},
+	},
+});
+
+app.mount("#events");
