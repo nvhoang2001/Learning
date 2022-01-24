2296d380d70a19fc6929eaccbdb680475dabf605
nvhoang2001 <ngoviethoang2424@gmail.com>
2022-01-24 09:31:11 +0700

    Removed commented code

--- store/chartData/mutations.js
+++ store/chartData/mutations.js
@@ -1,6 +1,6 @@
 const convertToPercentage = (array, limit) => {
     for (let i = 0; i < array.length; i++) {
-        array[i] = +(100 * (array[i] / limit)); //.toFixed(2);
+        array[i] = +(100 * (array[i] / limit)).toFixed(2);
     }
 };
 
@@ -10,34 +10,5 @@ export default {
         convertToPercentage(state.follower, state.foLimit);
         convertToPercentage(state.visitor, state.viLimit);
         state.converted = true;
-        // state.revenue.forEach((rev) =>
-        //     (100 * (rev / state.reLimit)).toFixed(2)
-        // );
-
-        // for (let i = 0; i < state.revenue.length; i++) {
-        //     state.revenue[i] = +(
-        //         100 *
-        //         (state.revenue[i] / state.reLimit)
-        //     ).toFixed(2);
-        // }
-        // for (let i = 0; i < state.follower.length; i++) {
-        //     state.follower[i] = +(
-        //         100 *
-        //         (state.follower[i] / state.foLimit)
-        //     ).toFixed(2);
-        // }
-        // for (let i = 0; i < state.visitor.length; i++) {
-        //     state.visitor[i] = +(
-        //         100 *
-        //         (state.visitor[i] / state.viLimit)
-        //     ).toFixed(2);
-        // }
-
-        // state.follower.forEach((fol) =>
-        //     (100 * (fol / state.foLimit)).toFixed(2)
-        // );
-        // state.visitor.forEach((vis) =>
-        //     (100 * (vis / state.viLimit)).toFixed(2)
-        // );
     },
 };
